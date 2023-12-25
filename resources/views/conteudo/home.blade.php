005fa9f558340f3d286803d62f23df5a36983e4a
Gustavo Prado <89040269+GustavoPrado20@users.noreply.github.com>
2023-12-25 15:25:25 -0300

    front-end header
    criando o header da aplicação

---
+++ resources/views/conteudo/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.esqueleto')
+
+@section('styles')
+
+@endsection
+
+@section('conteudo')
+    <main>
+        
+    </main>
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <h2 class="logo">Su<span>cata</span></h2>
+
+    <nav class="navLinks">
+        <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Membros</a></li>
+            <li><a href="">Jogos</a></li>
+            <li><a href="">Finanças</a></li>
+            <li><a href="">Estatisticas</a></li>
+        </ul>
+    </nav>
+</header>
