1faac28e01fc230a0e8bf9485798e93fbea18a0e
Gustavo Prado <89040269+GustavoPrado20@users.noreply.github.com>
2024-02-29 20:05:13 -0300

    add icone bola

--- resources/views/layouts/esqueleto.blade.php
+++ resources/views/layouts/esqueleto.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+        <link rel="icon" href="{{ asset('img/icones/icons8-bola-de-futebol-2-20.png') }}" type="image/x-icon">
+
         <title>Sucata</title>
         
         <link rel="stylesheet" href="{{ asset('css/main.css') }}">
