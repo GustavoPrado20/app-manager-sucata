e4288cc054f59248e5d6f68e8ad00b60cb1306e3
Gustavo Prado <89040269+GustavoPrado20@users.noreply.github.com>
2025-03-24 18:01:25 -0300

    Update finanças.blade.php

--- resources/views/conteudo/finanças.blade.php
+++ resources/views/conteudo/finanças.blade.php
@@ -28,7 +28,7 @@
 
                         <section class="titulo-card">
                             <h3>Receitas</h3>
-                            <p>R$ {{ $receitas}},00 - 500,00</p>
+                            <p>R$ {{ $receitas - 500,00}},00</p>
                         </section>
                     </section>
 
